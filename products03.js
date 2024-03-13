5ba3e8207e27a9f3225b3c5c02dea49d573bad4a
Dario Argüello <arguellodario1@gmail.com>
2024-03-13 19:45:49 -0300

    desafio03

--- products03.js
+++
@@ -1,3 +0,0 @@
-const moment = require ("moment");
-
-console.log(moment().format("DD/MM/YYYY H:m:s"));
--- src/app.js
+++ src/app.js
@@ -1,3 +1,33 @@
 import express from 'express';
 import ProductManager from './products03';
 
+const app = express();
+const PORT = 8080; 
+
+const manager = new ProductManager(`products.json`); 
+
+
+app.get('/products', async (request, response) => {
+    let products = await manager.getProducts();
+    const {limit} = request.query;
+
+    if(limit){
+        products = products.slice(0, limit);
+    }
+
+    response.send(products);
+});
+
+app.get('/products/:productId', async (request, response) => {
+    const productId = request.params.productId;
+
+    let product = await manager.getProductById(productId);
+
+    product ? response.send(product) : console.error('Producto no encontrado');
+
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor activo en http://localhost:${PORT}`);
+})  
